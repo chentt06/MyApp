fb2aa79cd98f8122aa2ca1be1f6ffb085742bf37
chentt06 <trinacheng43@gmail.com>
2025-11-15 17:45:11 -0500

    he

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'One',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
